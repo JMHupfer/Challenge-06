81504a02036da4b79fbf43554e9a21b3832b1648
JMHupfer <jmhupfer@gmail.com>
2023-04-02 19:08:35 -0600

    second commit with more js, css, html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,18 +1,55 @@
-var APIKey = "27188ffde4e8c2e5bf4b2ab0515b5ddc";
-var city;
+var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc"
+var requestUrlGeo = "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc"
+var apiKey = "27188ffde4e8c2e5bf4b2ab0515b5ddc"
+var city = []
+var inputElement = document.querySelector("#searchcity")
+var searchButton = document.querySelector(".search")
 
-function weatherByCity(city){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
-    .then(function (response) { 
+console.log(searchButton)
+
+searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(inputElement.value);
+    getCurrentWeather(inputElement.value);
+}) 
+
+function getCurrentWeather(city){
+   var URL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc" 
+   fetch(URL)
+   .then(function (response) {
+       return response.json();
+   })
+   .then(function (data) {
+       console.log(data[0].lat);
+       console.log(data[0].lon);
+       getWeather(data[0].lat, data[0].lon)
+   });
+}
+
+function getWeather(lat, lon){
+    var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc&units=metric"
+    fetch(URL)
+    .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(response);
+        console.log(data);
     })
-    .catch(function() {
-    })    
 }
 
-window.onload = function() {
-    weatherByCity( 6167865 );
-}
+// var cityTemp = document.getElementById("temp").textContent
+// var cityHumidity = document.getElementById("humidity").textContent
+// var cityWind = document.getElementById("wind").textContent
+
+// var cityTemp2 = document.getElementById("temp2").textContent
+// var cityHumidity2 = document.getElementById("humidity2").textContent
+// var cityWind2 = document.getElementById("wind2").textContent
+
+document.getElementById("temp").textContent = data.temp
+
+
+
+
+
+
+
