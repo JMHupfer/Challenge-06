c680a49c8aa28d0ca0e822549c1b625d48399574
JMHupfer <jmhupfer@gmail.com>
2023-04-03 21:09:45 -0600

    javascript run through

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,41 +5,61 @@ var city = []
 var inputElement = document.querySelector("#searchcity")
 var searchButton = document.querySelector(".search")
 
-searchButton.addEventListener("click", function(event) {
+searchButton.addEventListener("click", function (event) {
     event.preventDefault();
     console.log(inputElement.value);
     getCurrentWeather(inputElement.value);
-}) 
+})
 
-function getCurrentWeather(city){
-   var URL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc" 
-   fetch(URL)
-   .then(function (response) {
-       return response.json();
-   })
-   .then(function (data) {
-       console.log(data[0].lat);
-       console.log(data[0].lon);
-       getWeather(data[0].lat, data[0].lon)
-   });
+function getCurrentWeather(city) {
+    var URL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc"
+    fetch(URL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data[0].lat);
+            console.log(data[0].lon);
+            getWeather(data[0].lat, data[0].lon)
+        });
 }
 
-function getWeather(lat, lon){
+function getWeather(lat, lon) {
     var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc&units=metric"
     fetch(URL)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-        document.getElementById("temp").textContent = data.main.temp
-        document.getElementById("humidity").textContent = data.main.humidity
-        document.getElementById("wind").textContent = data.main.wind
-        document.getElementById("temp2").textContent = data.main.temp
-        document.getElementById("humidity2").textContent = data.main.humidity
-        document.getElementById("wind2").textContent = data.main.wind
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            document.getElementById("temp").textContent = data.main.temp
+            document.getElementById("humidity").textContent = data.main.humidity
+            document.getElementById("wind").textContent = data.wind.speed
+        });
+        
+        var URL2 = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=27188ffde4e8c2e5bf4b2ab0515b5ddc&units=metric"
+        fetch(URL2)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var forecast = data.list
+            console.log(forecast[3]);
+            console.log(forecast[11]);
+            console.log(forecast[19]);
+            console.log(forecast[27]);
+            console.log(forecast[35]);
+            // document.getElementById("temp2").textContent = data.main.temp
+            // document.getElementById("humidity2").textContent = data.main.humidity
+            // document.getElementById("wind2").textContent = data.wind.speed
+            // for (var i = 3; i < forecast.length; i + 6) {
+                //     const element = forecast[i];
+                //     console.log(element)
+                // }
+        })
 }
+
+
 
 
 
